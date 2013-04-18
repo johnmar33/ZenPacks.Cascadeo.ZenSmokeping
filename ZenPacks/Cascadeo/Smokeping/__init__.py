3f937637007a439dd8ccf0adff1076f33a98d61d
Josephson Eduardo <josephson@cascadeo.com>
2013-04-18 07:43:40 +0000

    Committing changes to __init__.py

--- ZenPacks/Cascadeo/Smokeping/__init__.py
+++ ZenPacks/Cascadeo/Smokeping/__init__.py
@@ -106,6 +106,8 @@ def createSPDashboard(self, REQUEST=None):
         raise Unauthorized("Unauthorized")
 
 
+    # 04/02/2013 - temporary
+    import os
     html = ""
 
     # Get urls of target dashboard
@@ -125,51 +127,113 @@ def createSPDashboard(self, REQUEST=None):
 
     # Fetch target dashboard webpage to trigger smokeping to create latest images
     for url in urls.keys():
-###        # temporary - Josephson
-###        # to accomodate Smokeping auth - Josephson
-###        os.system("echo \".urllib2.urlopen(\"%s\")\" >> /usr/local/zenoss/zenoss/log/event.log" % url)
         site = urllib2.urlopen(url)
             
     # Generate Dasboard for Zenoss
-    html += "<html><body><div id=\"zensmokeping_dashboard\"><ul>"
+    html = html + "<html>"
+    html = html + "<script type=\"text/javascript\" src=\"zport/jsDashboard\"></script>"
+    html = html + "<body>"
+
+    html = html + "<div id=\"zensmokeping_filter\">"
+    html = html + "<input id=\"device_name\" name=\"device_name\" maxlength=\"32\" />"
+    html = html + ("<button name=\"filter_device_btn\" id=\"filter_device_btn\" onclick=\"filter_device();\">Filter</button>")
+    html = html + "</div>"
+    html = html + "<div id=\"zensmokeping_dashboard\"><ul>"
 
     orgs = sorted(self.dmd.Groups.Smokeping.getSubOrganizers(), key=lambda o:(o.id).upper())
     o = None
     for o in orgs:
-      if o != None:
-        html += "</ul></li>"
-      org_name = "/%s/%s" %(o.__primary_parent__.id, o.id)
-      html += "<li><strong>" + org_name + "</strong><ul>"
-
+      # 04/02/2013 - get sub-devices first to prevent "blank" Smokeping Groups from being displayed
       dvs = sorted(o.getSubDevices(), key=lambda d:d.id)
-      for d in dvs:
-            html += "<li>"
-            html += "<a target='_blank' href='" + d.zSmokepingPublicUrl + "smokeping.cgi?target="
-            html += d.zSmokepingTarget.replace('/', '.') + "'>" + d.getDeviceName() + "</a>"
-            html += "<img width='95%' src='/zport/fetchImage?url=" + d.zSmokepingPrivateUrl
-            html += "images/" + d.zSmokepingTarget + "_mini.png'/>"
-            html += "</li>"
 
+      if len(dvs) > 0:
+        if o != None:
+          html += "</ul></li>"
+        org_name = "/%s/%s" %(o.__primary_parent__.id, o.id)
+        html += "<li><strong>" + org_name + "</strong><ul>"
+  
+        for d in dvs:
+              html += "<li>"
+              html += "<a target='_blank' href='" + d.zSmokepingPublicUrl + "smokeping.cgi?target="
+              html += d.zSmokepingTarget.replace('/', '.') + "'>" + d.getDeviceName() + "</a>"
+              html += "<img width='95%' src='/zport/fetchImage?url=" + d.zSmokepingPrivateUrl
+              html += "images/" + d.zSmokepingTarget + "_mini.png'/>"
+              html += "</li>"
+  
     html += "</ul></div></body></html>"
 
     REQUEST.RESPONSE.setHeader('Content-Type','text/html')
     return html
 
-def createSample(self, REQUEST=None):
+def filterDevice(self, dev='/', REQUEST=None):
     """
-    sample new zensmokeping ZenPack page
+    For device filtering
     """
-    user = getSecurityManager().getUser().getUserName()
-    if user == 'Anonymous User':
-        raise Unauthorized("Unauthorized")
+    json_rs = {}
+    rs = self.dmd.Devices._findDevice("*%s*" %(dev))
+    if len(rs) == 0:
+        json_rs = {'results': "NO RESULTS"}
+    else:
+        orgs = sorted(self.dmd.Groups.Smokeping.getSubOrganizers(), key=lambda o:(o.id).upper())
+        o = None
+        json_rs = {'results': [], 'groups': [] }
+        # get organizers as well
+        for o in orgs:
+            row = {'parent_group': o.__primary_parent__.id, 'group': o.id, 'group_name': "/%s/%s" %(o.__primary_parent__.id, o.id)}
+            json_rs['groups'].append(row)
+        row = None
+        for d in rs:
+            if len(d.getObject().groups()) > 0:
+              smokeping_group = "/%s/%s" %(d.getObject().groups()[0].__primary_parent__.id, d.getObject().groups()[0].id)
+              row = {'device_id': d.getObject().id, 'device_name': d.getObject().getDeviceName(), 'zSmokepingTarget': d.getObject().zSmokepingTarget, 'zSmokepingPublicUrl': d.getObject().zSmokepingPublicUrl, 'zSmokepingPrivateUrl': d.getObject().zSmokepingPrivateUrl, 'smokeping_group': smokeping_group}
+              json_rs['results'].append(row)
 
-    html = "<html><body>Hello world ok? from createSample</body></html>"
-    REQUEST.RESPONSE.setHeader('Content-Type','text/html')
-    return html
+        json_rs['results'] = sorted(json_rs['results'], key=lambda o:o['smokeping_group'].upper())
+    import json
+    return json.dumps(json_rs)
 
+def jsDashboard(self, REQUEST=None):
+    """
+    JS for the createSPDashboard Portlet
+    """
+    js_code = "function filter_device() {\n" \
+    "var xmlHttp;\n" \
+    "dev = document.getElementById('device_name').value; \n" \
+    "url = \"zport/filterDevice?dev=\"+dev; \n" \
+    "if (window.XMLHttpRequest) { \n" \
+    "xmlHttp = new XMLHttpRequest(); \n" \
+    "} else { \n" \
+    "xmlHttp = new ActiveXObject(\"Microsoft.XMLHTTP\"); \n" \
+    "} \n" \
+    "xmlHttp.open(\"GET\", url, true); \n" \
+    "xmlHttp.send(); \n" \
+    "xmlHttp.onreadystatechange = function() { \n" \
+    "\tif (xmlHttp.readyState == 4 && xmlHttp.status == 200) { \n" \
+    "\t\tvar json = eval('(' + xmlHttp.responseText + ')'); \n" \
+    "\t\tdashboard_div = document.getElementById('zensmokeping_dashboard'); \n" \
+    "\t\tdashboard_div.innerHTML = \"\"; \n" \
+    "\t\tcontent = \"\"; \n" \
+    "\t\tprev_group = \"\"; // previous smokeping group handler \n" \
+    "\t\tif (json.results.length > 0) content = content + \"<ul>\"; \n" \
+    "\t\tfor (i = 0; i < json.results.length; i++) {\n" \
+    "\t\t\tif(json.results[i].smokeping_group != prev_group) { \n" \
+    "\t\t\t\tprev_group = json.results[i].smokeping_group; \n" \
+    "\t\t\t\tif (prev_group != \"\") content = content + \"</ul>\"; \n" \
+    "\t\t\t\tcontent = content + \"<li><strong>\" + json.results[i].smokeping_group + \"</strong></li><ul>\"; \n" \
+    "\t\t\t} \n" \
+    "\t\t\tcontent = content + \"<li><a target='_blank' href='\" + json.results[i].zSmokepingPublicUrl + \"smokeping.cgi?target=\" + json.results[i].zSmokepingTarget + \"'>\" + json.results[i].device_name + \"</a>\";  \n" \
+    "\t\t\tcontent = content + \"<img width='95%' src='/zport/fetchImage?url=\" + json.results[i].zSmokepingPrivateUrl + \"images/\" + json.results[i].zSmokepingTarget + \"_mini.png' /></li>\"; \n" \
+    "\t\t} \n" \
+    "\t\tif (json.results.length > 0) content = content + \"</ul>\"; \n" \
+    "\t\tdashboard_div.innerHTML += content; \n" \
+    "\t} \n" \
+    "} \n" \
+    "}\n"
+    return js_code
 
 from Products.ZenModel.ZentinelPortal import ZentinelPortal
 ZentinelPortal.fetchImage = fetchImage
 ZentinelPortal.createSPDashboard = createSPDashboard
 ZentinelPortal.updateSPDevice = updateSPDevice
-ZentinelPortal.createSample = createSample
+ZentinelPortal.filterDevice = filterDevice
+ZentinelPortal.jsDashboard = jsDashboard
